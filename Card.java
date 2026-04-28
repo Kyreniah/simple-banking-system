40e2a2e4b96991dfee0a2edf390e912f9972965f
Kyrenia <77701345+Kyreniah@users.noreply.github.com>
2026-04-28 19:05:56 +0200

    Fix card number generation

--- Card.java
+++ Card.java
@@ -3,17 +3,21 @@ package banking;
 import java.util.Random;
 
 public class Card {
-    int number;
+    long number;
     int pin;
 
-    public Card(int number, int pin) {
+    public Card(long number, int pin) {
         this.number = number;
         this.pin = pin;
     }
 
     static Card generateCard() {
         Random rnd = new Random();
-        int number = rnd.nextInt(9999);
+        String str = "400000";
+        for (int i = 0; i < 10; i++) {
+            str += Integer.toString(rnd.nextInt(9));
+        }
+        long number = Long.parseLong(str);
         int pin = rnd.nextInt(9999);
         return new Card(number, pin);
     }
